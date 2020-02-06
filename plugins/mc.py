e597a8932cd5c30ddecfdff5b017475612f96532
Anton Samokhvalov <anton@samokhvalov.xyz>
2020-02-06 15:40:22 +0000

    A

--- plugins/mc.py
+++ plugins/mc.py
@@ -1,12 +1,13 @@
-def mc(gui):
+def mc(gui, gui_lib):
     return {
         'code': """
              source fetch "http://ftp.midnight-commander.org/mc-{version}.tar.xz" 1
+             export LDFLAGS="$LDFLAGS $LIBS"
              $YSHELL ./configure $COFLAGS --prefix=$IDIR --disable-shared --enable-static --with-screen={gui}  || exit 1
              $YMAKE -j $NTHRS
              echo 'al install:' > doc/hlp/Makefile
              $YMAKE install
-        """.replace('{gui}', gui),
+        """.replace('{gui}', gui_lib),
         'version': '4.8.23',
         'meta': {
             'kind': ['program'],
@@ -17,9 +18,9 @@ def mc(gui):
 
 @y.package
 def mc_slang0():
-    return mc('slang')
+    return mc('slang', 'slang')
 
 
 @y.package
 def mc_ncurses0():
-    return mc('ncurses-real')
+    return mc('ncurses', 'ncurses')
