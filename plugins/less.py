aa35d9d69b281fb1b63849a2b4766ec978f1ed8b
Anton Samokhvalov <anton@samokhvalov.xyz>
2020-02-12 14:09:25 +0300

    A

--- plugins/less.py
+++ plugins/less.py
@@ -3,6 +3,8 @@ def less0():
     return {
         'code': """
              source fetch "http://www.greenwoodsoftware.com/less/less-{version}.tar.gz" 1
+             export LDFLAGS="$LDFLAGS $LIBS"
+             export CFLAGS="-Dwinch=winch_ $CFLAGS"
              $YSHELL ./configure $COFLAGS --prefix=$IDIR --enable-static --disable-shared || exit 1
              $YMAKE -j $NTHRS
              $YMAKE install
@@ -10,6 +12,7 @@ def less0():
         'version': '551',
         'meta': {
             'kind': ['tool'],
+            'depends': ['ncurses'],
             'profides': [
                 {'env': 'LESS', 'value': '{pkgroot}/bin/less'}
             ],
