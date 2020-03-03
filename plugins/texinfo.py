e306f83c80534ead0bc1f88838567001af04dbbf
Anton Samokhvalov <anton@samokhvalov.xyz>
2020-03-04 00:06:17 +0300

    A

--- plugins/texinfo.py
+++ plugins/texinfo.py
@@ -3,12 +3,13 @@ def texinfo0():
     return {
         'code': """
              source fetch "http://ftp.gnu.org/gnu/texinfo/texinfo-{version}.tar.xz" 1
+             export PERL="$YPERL"
              $YSHELL ./configure $COFLAGS --prefix=$IDIR --enable-static --disable-shared || exit 1
              $YMAKE -j $NTHRS
              $YMAKE install
         """,
         'meta': {
-            'depends': ['c++', 'make', 'c'],
+            'depends': ['c++', 'make', 'c', 'perl5'],
             'provides': [
                 {'tool': 'MAKEINFO', 'value': '{pkgroot}/bin/makeinfo'},
             ],
