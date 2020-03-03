4e1973fd273eadd1b8295c8bd6a8c9ddc7a28ff0
Anton Samokhvalov <anton@samokhvalov.xyz>
2020-03-03 23:04:42 +0300

    A

--- plugins/dlmalloc.py
+++ plugins/dlmalloc.py
@@ -3,7 +3,7 @@ def dlmalloc0():
     return {
         'code': '''
             source fetch "https://raw.githubusercontent.com/ennorehling/dlmalloc/master/malloc.c" 0
-            $CC -DUSE_MALLOC_LOCK=1 -c ./malloc.c -o malloc.o
+            $CC $CFLAGS -DUSE_MALLOC_LOCK=1 -c ./malloc.c -o malloc.o
             $AR q libdlmalloc.a malloc.o
             $RANLIB libdlmalloc.a
             mkdir $IDIR/lib
