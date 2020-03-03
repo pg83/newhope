bb9868e41a901c432bbf66b971d895b681d4e5e6
Anton Samokhvalov <anton@samokhvalov.xyz>
2020-03-03 22:40:30 +0300

    A

--- plugins/ptmalloc.py
+++ plugins/ptmalloc.py
@@ -5,7 +5,7 @@ def ptmalloc30():
             source fetch "http://www.malloc.de/malloc/ptmalloc3-current.tar.gz" 0
             mv ptmalloc* xxx
             mv xxx/* ./
-            $YMAKE CC="$CC" AR="$AR" RANLIB="$RANLIB" OPT_CFLAGS="$CFLAGS" libptmalloc3.a
+            $YMAKE CC="$CC $CFLAGS" AR="$AR" RANLIB="$RANLIB" OPT_CFLAGS="$CFLAGS" libptmalloc3.a
             mkdir $IDIR/lib
             mv libptmalloc3.a $IDIR/lib/
         ''',
