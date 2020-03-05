ae74920f2bd83637ac9c16d19d1a61bf50e9f546
Anton Samokhvalov <anton@samokhvalov.xyz>
2020-03-05 16:31:55 +0300

    A

--- images/build/run.sh
+++ images/build/run.sh
@@ -4,4 +4,4 @@ docker volume create build-root
 docker volume create build-build
 docker volume create build-storage
 
-exec docker run -ti -v build-root:/root -v build-build:/media/build -v build-storage:/media/storage $@
+exec docker run -ti --net=host -v build-root:/root -v build-build:/media/build -v build-storage:/media/storage $@
